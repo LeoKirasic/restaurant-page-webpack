277602d1059ee028090a99782d4ac99619643963
Leo Kirasic <kirasicleo@gmail.com>
2021-08-28 18:13:49 +0200

    set up tabbed browsing

--- src/home.js
+++ src/home.js
@@ -1,26 +1,7 @@
-import css from './style.css';
 
 const loadHomePage = () => {
 
-const mainDiv = document.querySelector('#main');
-const header = document.createElement('header');
-const titleDiv = document.createElement('div');
-titleDiv.id = 'title';
-titleDiv.textContent = `Leo's Sushi`;
-const ul = document.createElement('ul');
-const home = document.createElement('li');
-home.textContent = 'Home';
-const menu = document.createElement('li');
-menu.textContent = 'Menu';
-const about = document.createElement('li');
-about.textContent = 'About';
-
-header.appendChild(titleDiv);
-ul.appendChild(home);
-ul.appendChild(menu);
-ul.appendChild(about);
-header.appendChild(ul);
-mainDiv.appendChild(header);
+ const mainDiv = document.querySelector('#main');
 
 const contentContainer = document.createElement('div');
 contentContainer.id = 'content-container';
--- src/index.js
+++ src/index.js
@@ -3,4 +3,44 @@ import { loadHomePage } from './home';
 import { loadMenuPage } from './menu';
 import { loadAboutPage } from './about';
 
-loadAboutPage();
+const mainDiv = document.querySelector('#main');
+const header = document.createElement('header');
+const titleDiv = document.createElement('div');
+titleDiv.id = 'title';
+titleDiv.textContent = `Leo's Sushi`;
+const ul = document.createElement('ul');
+const home = document.createElement('li');
+home.textContent = 'Home';
+const menu = document.createElement('li');
+menu.textContent = 'Menu';
+const about = document.createElement('li');
+about.textContent = 'About';
+
+header.appendChild(titleDiv);
+ul.appendChild(home);
+home.id = 'home';
+ul.appendChild(menu);
+menu.id = 'menu';
+ul.appendChild(about);
+about.id = 'about';
+header.appendChild(ul);
+mainDiv.appendChild(header);
+
+loadHomePage();
+
+home.addEventListener('click', ()=>{
+    const contentContainer = document.querySelector('#content-container');
+    contentContainer.remove();
+    loadHomePage();
+    
+});
+menu.addEventListener('click', ()=>{
+    const contentContainer = document.querySelector('#content-container');
+    contentContainer.remove();
+    loadMenuPage();
+});
+about.addEventListener('click', ()=>{
+    const contentContainer = document.querySelector('#content-container');
+    contentContainer.remove();
+    loadAboutPage();
+});
